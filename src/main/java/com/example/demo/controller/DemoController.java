41dc723bae47f4431dafcf3b288f2f5807b8a46d
Dishang Patel <dishangpatel@asite.com>
2026-04-07 18:36:45 +0530

    QA2

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -24,4 +24,9 @@ public class DemoController {
     public Greeting req(HttpServletRequest request, @RequestPart(name = "file", required = false) MultipartFile multipartFile) {
         return new Greeting(counter.incrementAndGet(), String.format("Hello, %s!", request.getParameter("name")));
     }
+
+    @GetMapping("/QA2")
+    public String QA2() {
+        return "QA2";
+    }
 }
